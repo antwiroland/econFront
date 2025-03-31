e449980e9237b822425f577e108eeb928d3d9d14
antwiroland <antwiroland56@gmail.com>
2025-03-31 14:08:24 +0000

    change current to dollar frontend

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -34,7 +34,7 @@ const ProductCard = ({ product }) => {
         <div className="mt-2 mb-2 flex items-center justify-between">
           <p>
             <span className="text-3xl font-bold text-emerald-400">
-              GHS&nbsp;{product.price}
+              $&nbsp;{product.price}
             </span>
           </p>
         </div>
