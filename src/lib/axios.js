792f86a50f90bb2e04b6866f36a7b1842f15513c
antwiroland <antwiroland56@gmail.com>
2025-03-31 14:22:12 +0000

    edit backend live url to frontend

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "https://econbackend.onrender.com/api" : "/api",
-	withCredentials: true, // send cookies to the server
+  // baseURL: import.meta.mode === "development" ? "https://econbackend.onrender.com/api" : "/api",
+  baseURL: import.meta.mode === "https://econbackend.onrender.com/api",
+  withCredentials: true, // send cookies to the server
 });
 
 export default axiosInstance;
